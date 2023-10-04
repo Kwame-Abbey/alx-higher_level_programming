944ad9090fd4f2f4171809a1274b513fc7a69dab
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-04 01:57:43 +0000

    Corrected task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,6 +20,7 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	if (*head == NULL || (*head)->n >= new_node->n)
 	{
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
