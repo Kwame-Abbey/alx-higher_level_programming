0a9aafd4d8c56c3e7ae6eee2a54f2c76ebd27884
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-05 13:56:54 +0000

    update file

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,15 +4,10 @@ from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
     """returns the sum or difference"""
-    add = None
-    sub = None
-    c = None
-
     if a < b:
-        add = globals()['add']
         c = add(a, b)
 
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
 
         return c
