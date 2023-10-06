6be8d0d291465ee8231d12d34c7cf77cad55c5de
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-06 08:49:24 +0000

    Prints in reverse form the elements of a list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list in reverse order"""
+    list_length = len(my_list)
+    for i in range(list_length - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
