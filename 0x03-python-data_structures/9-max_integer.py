4e35654d69b260a51373388a8989acacc63914e9
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-06 12:23:48 +0000

    finds biggest integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """finds the biggest integer of a list"""
+    if len(my_list) == 0:
+        return None
+    int_max = 0
+    for num in my_list:
+        if num > int_max:
+            int_max = num
+    return int_max
