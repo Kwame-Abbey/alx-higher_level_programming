1e16075c2261135bef61740ab821b0ab03062a21
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-07 06:39:11 +0000

    Initialized int_max to first element rather than 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     """finds the biggest integer of a list"""
     if len(my_list) == 0:
         return None
-    int_max = 0
+    int_max = my_list[0]
     for num in my_list:
         if num > int_max:
             int_max = num
