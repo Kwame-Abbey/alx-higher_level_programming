7459e31ca394afa3ace7bca2e5a2d0c52a0c4056
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-11 09:05:07 +0000

    Working with dictionaries

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """prints a dictionary by ordered keys"""
+    if a_dictionary:
+        my_keys = list(a_dictionary.keys())
+        my_keys.sort()
+        for key in my_keys:
+            print(f"{key}: {a_dictionary[key]}")
+    else:
+        pass
