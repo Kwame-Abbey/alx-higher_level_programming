a7249dc530b6e01d871862af98c03a39fbdaaed1
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-23 08:06:29 +0000

    safe print 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """ prints x elements of a list"""
+    length = 0
+    should_break = False
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            length += 1
+        except IndexError:
+            should_break = True
+        if should_break:
+            break
+    print()
+    return length
