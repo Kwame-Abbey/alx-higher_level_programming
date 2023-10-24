8fd89dd84b7eaa60186185fb72724927f1542e9e
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-24 09:33:52 +0000

    Added public instance method area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Defines a blueprint for squares"""
+
+    def __init__(self, size=0):
+        """magic function that initializes once an instance is created"""
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
