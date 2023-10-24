6779a0aee0561a1fef6bec2e09d60ab1ae7a4ba9
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-24 09:26:47 +0000

    fix issues in task 1 and task 0, also finished task 2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a square"""
+
+
 class Square:
-    """Defines a square"""
+    """Defines an empty class square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Defines a square"""
+
+
 class Square:
-    """Defines a square"""
+    """Defines a square class with private instance attribute size"""
 
     def __init__(self, size):
+        """magic function that initializes once an instance is created"""
         self.__size = size
