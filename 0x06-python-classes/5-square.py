b0b587b44462b03ab3f614dc528400f1c6acc4bb
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-10-24 09:54:29 +0000

    Add new public instance method print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Defines a blueprint for squares"""
+
+    def __init__(self, size=0):
+        """magic function that initializes once an instance is created"""
+        self.size = size
+
+    @property
+    def size(self):
+        """get the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size after verifying certain conditions"""
+        if type(value) is int:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """returns current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
