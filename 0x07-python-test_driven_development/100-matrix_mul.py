152beb9d6d9fe3cc500d64830c508a3b93394098
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-01 13:59:43 +0000

    Correct exception messages

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,13 +22,13 @@ def matrix_mul(m_a, m_b):
         raise ValueError('m_b can\'t be empty')
     for row in m_a:
         if len(row) != len_a:
-            raise TypeError('each row of m_a must should be of the same size')
+            raise TypeError('each row of m_a must be of the same size')
         for i in row:
             if type(i) is not int and type(i) is not float:
                 raise TypeError('m_a should contain only integers or floats')
     for row in m_b:
         if len(row) != len_b:
-            raise TypeError('each row of m_b must should be of the same size')
+            raise TypeError('each row of m_b must be of the same size')
         for i in row:
             if type(i) is not int and type(i) is not float:
                 raise TypeError('m_b should contain only integers or floats')
