bba0cae9651708b41fb3de76fc79b7daf7e976ee
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-07 09:16:00 +0000

    load form a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines creating am object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an object from a json file
+
+    Args:
+        filename: name of file
+    """
+    with open(filename, 'r', encoding='utf-8') as data_file:
+        json.load(data_file)
