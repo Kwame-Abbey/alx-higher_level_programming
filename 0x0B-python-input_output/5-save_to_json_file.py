8deebbcb9c7cb88e6f3ba743536aa57059a8a5af
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-07 09:06:41 +0000

    json dump

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+"""Defines writing to a text file using JSON representation"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an object to a text file using JSON
+
+    Args:
+        my_obj: object
+        filename: file object
+    """
+    with open(filename, 'w', encoding='utf-8') as data_file:
+        json.dump(my_obj, data_file)
