c5731a6e145ca486808f628be402d777fb2fe893
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-10 08:33:34 +0000

    list dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a class Base"""
 
+import json
+
 
 class Base:
     """
@@ -16,3 +18,9 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
