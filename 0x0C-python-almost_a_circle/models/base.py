bff74c429b8ac5fc5951892de79ec1c26bfe864a
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-10 11:47:06 +0000

    Use csv

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Defines a class Base"""
 
 import json
+import csv
 
 
 class Base:
@@ -67,3 +68,36 @@ class Base:
         except FileNotFoundError:
             pass
         return new_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ serializes in csv format"""
+        filename = cls.__name__ + ".csv"
+        if cls.__name__ is "Rectangle":
+            attrs = ["width", "height", "x", "y", "id"]
+        elif cls.__name__ is "Square":
+            attrs = ["size", "x", "y", "id"]
+        with open(filename, 'w', newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=attrs)
+            if list_objs is not None:
+                writer.writeheader()
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+            else:
+                writer.writerow([[]])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ deserializes in csv format """
+        filename = cls.__name__ + ".csv"
+        new_list = []
+        try:
+            with open(filename, 'r', newline="") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    for k, v in row.items():
+                        row[k] = int(v)
+                    new_list.append(row)
+            return [cls.create(**obj) for obj in new_list]
+        except FileNotFoundError:
+            return [[]]
