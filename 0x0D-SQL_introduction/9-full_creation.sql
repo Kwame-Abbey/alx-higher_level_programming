956951c2cfaa79faf5e41441fe17d723924efc2e
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-12-12 16:18:25 +0000

    Creates second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Creates a table named 'second_table'
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+-- Adds multiple rows
+INSERT INTO second_table
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
