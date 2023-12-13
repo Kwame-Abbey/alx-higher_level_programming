057a358ca87df0d2e43a79649944779894d613c0
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-12-13 19:36:59 +0000

    fix how to reference database in creation of table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- Creates a database 'hbtn_0d_usa'
 -- Creates a table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
 );
