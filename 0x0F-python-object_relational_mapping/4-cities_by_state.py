b5b3b26fab7814f8c28a5309e208d501317d0a5a
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-01-12 16:11:34 +0000

    add order by statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     cur.execute("SELECT cities.id, cities.name, states.name \
                 FROM states \
                 INNER JOIN cities \
-                ON states.id = cities.state_id")
+                ON states.id = cities.state_id \
+                ORDER BY cities.id ASC")
 
     rows = cur.fetchall()
     for row in rows:
