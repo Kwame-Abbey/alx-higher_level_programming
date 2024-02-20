51714befea7c0ac8fba04b4342fe2e6696a12b7e
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-20 17:22:04 +0000

    fix issues

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,36 +1,20 @@
 #!/usr/bin/node
-
-const { argv } = require("node:process");
-const request = require("request");
-
-const filmId = argv[2];
-const endpoint = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
-
-request.get({ url: endpoint }, (err, response, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  } else if (response.statusCode === 200) {
-    let charDict = {};
-    const characters = JSON.parse(data).characters;
-
-    characters.forEach((character) => {
-      let characterUrl = character;
-      request.get({ url: characterUrl }, (err, response, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        } else if (response.statusCode === 200) {
-          charDict[character] = JSON.parse(data).name;
-        }
-
-        if (characters.length === Object.keys(charDict).length) {
-          for (let key in charDict) {
-            console.log(charDict[key]);
-          }
-        }
-      });
-    });
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
 
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
