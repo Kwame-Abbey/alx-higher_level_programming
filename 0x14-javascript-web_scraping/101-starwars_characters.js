123476b18ff20dcb4c4d51b9482cf57419e2073a
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-20 17:04:58 +0000

    fix task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-const request = require('request');
+const { argv } = require("node:process");
+const request = require("request");
 
 const filmId = argv[2];
 const endpoint = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
@@ -9,18 +9,28 @@ const endpoint = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 request.get({ url: endpoint }, (err, response, data) => {
   if (err) {
     console.error(err);
+    return;
   } else if (response.statusCode === 200) {
+    let charDict = {};
     const characters = JSON.parse(data).characters;
 
     characters.forEach((character) => {
-      const characterUrl = character;
+      let characterUrl = character;
       request.get({ url: characterUrl }, (err, response, data) => {
         if (err) {
           console.error(err);
+          return;
         } else if (response.statusCode === 200) {
-          console.log(JSON.parse(data).name);
+          charDict[character] = JSON.parse(data).name;
+        }
+
+        if (characters.length === Object.keys(charDict).length) {
+          for (let key in charDict) {
+            console.log(charDict[key]);
+          }
         }
       });
     });
   }
 });
+
